d6689e1d350038e9aeb36e90983cf7b182accf7b
Mariam-Kabu <mariamkabulovi@gmail.com>
2021-08-30 12:25:28 +0400

    burger bar

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ let navbarLinks = document.getElementById('navbarLinks');
 toggleButton.addEventListener('click', function(){
   navbarLinks.classList.toggle('open');
 })
+
+const menuBtn = document.querySelector('.burger-bar-div');
+let menuOpen = false;
+menuBtn.addEventListener('click', ()=>{
+  if(!menuOpen){
+    menuBtn.classList.add('opened');
+    menuOpen = true;
+  }else{
+    menuBtn.classList.remove('opened');
+    menuOpen = false;
+  }
+});
